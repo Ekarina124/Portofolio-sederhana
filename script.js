d0cda0e0197a4e141aa2556c48bce50f36967943
Ekarina124 <sitiekarinaseptianor@gmail.com>
2025-03-07 20:54:29 +0800

    Menambahkan Footer dan  menampilkan project dengan javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("project").innerHTML += `
+        <p>Daftar project:</p> 
+        <ul>
+            <li>Portfolio Website</li>
+            <li>To-Do List</li>
+            <li>Weather App</li>
+            <li>Calculator</li>
+            <li>Simple Blog</li>
+        </ul>
+    `;
+}); 
